2630ce4d0c460e7b4ccaede957fcc2ecb2217ab5
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2024-08-05 20:27:54 +0400

    ajax

--- js/script.js
+++ js/script.js
@@ -17,83 +17,83 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // чек-листы одиночный выбор
 
-  const listItems2 = document.querySelectorAll('.form .chek-lists-single li');
+  // const listItems2 = document.querySelectorAll('.form .chek-lists-single li');
 
-  listItems2.forEach(function(item) {
-    item.addEventListener('click', function() {
-      // Снять класс active со всех пунктов списка
-      listItems2.forEach(function(el) {
-        el.classList.remove('active');
-      });
-      // Добавить класс active к выбранному пункту
-      item.classList.add('active');
-    });
-  });
+  // listItems2.forEach(function(item) {
+  //   item.addEventListener('click', function() {
+  //     // Снять класс active со всех пунктов списка
+  //     listItems2.forEach(function(el) {
+  //       el.classList.remove('active');
+  //     });
+  //     // Добавить класс active к выбранному пункту
+  //     item.classList.add('active');
+  //   });
+  // });
 
-  // чек-листы мультивыбор
+  // // чек-листы мультивыбор
 
-  const listItems = document.querySelectorAll('.form .chek-lists li');
+  // const listItems = document.querySelectorAll('.form .chek-lists li');
 
-  listItems.forEach(function(item) {
-    item.addEventListener('click', function() {
-      item.classList.toggle('active');
-    });
-  });
+  // listItems.forEach(function(item) {
+  //   item.addEventListener('click', function() {
+  //     item.classList.toggle('active');
+  //   });
+  // });
 
-  //обработка числовых полей
+  // //обработка числовых полей
   
-  const numberInputs = document.querySelectorAll('.number-input');
+  // const numberInputs = document.querySelectorAll('.number-input');
 
-  numberInputs.forEach(function(input) {
-    function formatNumber(value) {
-      return value.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-    }
+  // numberInputs.forEach(function(input) {
+  //   function formatNumber(value) {
+  //     return value.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  //   }
 
-    function applyStyles() {
-      const parent = input.closest('.input-container');
-      const placeholder = parent.querySelector('.placeholder');
+  //   function applyStyles() {
+  //     const parent = input.closest('.input-container');
+  //     const placeholder = parent.querySelector('.placeholder');
 
-      if (input.value) {
-        input.classList.add('filled');
-        placeholder.classList.add('filled');
-      } else {
-        input.classList.remove('filled');
-        placeholder.classList.remove('filled');
-      }
-    }
+  //     if (input.value) {
+  //       input.classList.add('filled');
+  //       placeholder.classList.add('filled');
+  //     } else {
+  //       input.classList.remove('filled');
+  //       placeholder.classList.remove('filled');
+  //     }
+  //   }
 
-    input.addEventListener('input', function() {
-      // Remove all non-digit characters
-      let value = input.value.replace(/\s+/g, '').replace(/\D/g, '');
+  //   input.addEventListener('input', function() {
+  //     // Remove all non-digit characters
+  //     let value = input.value.replace(/\s+/g, '').replace(/\D/g, '');
 
-      // Ensure the value respects the min, max and maxlength constraints
-      const min = parseInt(input.getAttribute('min'), 10);
-      const max = parseInt(input.getAttribute('max'), 10);
-      const maxLength = parseInt(input.getAttribute('maxlength'), 10);
+  //     // Ensure the value respects the min, max and maxlength constraints
+  //     const min = parseInt(input.getAttribute('min'), 10);
+  //     const max = parseInt(input.getAttribute('max'), 10);
+  //     const maxLength = parseInt(input.getAttribute('maxlength'), 10);
 
-      // Limit the length of the input
-      if (maxLength && value.length > maxLength) {
-        value = value.slice(0, maxLength);
-      }
+  //     // Limit the length of the input
+  //     if (maxLength && value.length > maxLength) {
+  //       value = value.slice(0, maxLength);
+  //     }
 
-      // Only set value if it's within the min and max range and has maxLength digits
-      if (value && value.length === maxLength) {
-        const intValue = parseInt(value, 10);
-        if (intValue < min) {
-          value = min.toString();
-        } else if (intValue > max) {
-          value = max.toString();
-        }
-      }
+  //     // Only set value if it's within the min and max range and has maxLength digits
+  //     if (value && value.length === maxLength) {
+  //       const intValue = parseInt(value, 10);
+  //       if (intValue < min) {
+  //         value = min.toString();
+  //       } else if (intValue > max) {
+  //         value = max.toString();
+  //       }
+  //     }
 
-      input.value = value;
-      // Apply styles
-      applyStyles();
-    });
+  //     input.value = value;
+  //     // Apply styles
+  //     applyStyles();
+  //   });
 
-    // Initial check to add filled class if input is already filled
-    applyStyles();
-  });
+  //   // Initial check to add filled class if input is already filled
+  //   applyStyles();
+  // });
   
   
 
@@ -110,40 +110,46 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Функция для загрузки и отображения модальных окон
-  function loadModal(modalType) {
-    fetch("modals.html")
-      .then((response) => response.text())
-      .then((html) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, "text/html");
-        const modalContent = doc.getElementById(modalType).innerHTML;
+ // Функция для загрузки и отображения модальных окон
+function loadModal(modalType) {
+  fetch("modals.html")
+    .then((response) => response.text())
+    .then((html) => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const modalContent = doc.getElementById(modalType).innerHTML;
 
-        document.getElementById("modal-body").innerHTML = modalContent;
-        document.getElementById("modal").style.display = "flex"; // Показываем модальное окно
-        document.body.classList.add("hold"); // Добавляем класс hold для body
+      document.getElementById("modal-body").innerHTML = modalContent;
+      document.getElementById("modal").style.display = "flex"; // Показываем модальное окно
+      document.body.classList.add("hold"); // Добавляем класс hold для body
 
-        applyInputMask(); // Применяем маску после загрузки модального окна
-      })
-      .catch((error) => console.error('Error loading modal content:', error));
-  }
+      initializeDynamicElements(); // Инициализация после загрузки модального окна
+      applyInputMask(); // Применяем маску после загрузки модального окна
+    })
+    .catch((error) => console.error('Error loading modal content:', error));
+}
 
-  // Вызов Модальных окон
-  document.querySelectorAll(".trigger").forEach((item) => {
-    item.addEventListener("click", function () {
-      const modalType = this.getAttribute("data-modal-type"); // Например, 'formModal' или 'infoModal'
-      loadModal(modalType);
-    });
+// Вызов Модальных окон
+document.querySelectorAll(".trigger").forEach((item) => {
+  item.addEventListener("click", function () {
+    const modalType = this.getAttribute("data-modal-type"); // Например, 'formModal' или 'infoModal'
+    loadModal(modalType);
   });
+});
 
-  // Закрытие модального окна
-  const closeButton = document.querySelector(".close-button");
-  if (closeButton) {
-    closeButton.addEventListener("click", function () {
-      document.getElementById("modal").style.display = "none";
-      document.body.classList.remove("hold"); // Убираем класс hold для body
-    });
-  }
+// Закрытие модального окна
+const closeButton = document.querySelector(".close-button");
+if (closeButton) {
+  closeButton.addEventListener("click", function () {
+    document.getElementById("modal").style.display = "none";
+    document.body.classList.remove("hold"); // Убираем класс hold для body
+  });
+}
+
+// Инициализация при загрузке страницы
+document.addEventListener('DOMContentLoaded', function () {
+  initializeDynamicElements();
+});
 
   // Переключатели
   document.querySelectorAll(".toggle").forEach((toggle) => {
@@ -210,4 +216,87 @@ document.body.addEventListener("input", function (event) {
     }
   }
 });
+
+
+
+// дублирование для ajax
+
+function initializeDynamicElements() {
+  // чек-листы одиночный выбор
+  const listItems2 = document.querySelectorAll('.form .chek-lists-single li');
+
+  listItems2.forEach(function(item) {
+    item.addEventListener('click', function() {
+      // Снять класс active со всех пунктов списка
+      listItems2.forEach(function(el) {
+        el.classList.remove('active');
+      });
+      // Добавить класс active к выбранному пункту
+      item.classList.add('active');
+    });
+  });
+
+  // чек-листы мультивыбор
+  const listItems = document.querySelectorAll('.form .chek-lists li');
+
+  listItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      item.classList.toggle('active');
+    });
+  });
+
+  // обработка числовых полей
+  const numberInputs = document.querySelectorAll('.number-input');
+
+  numberInputs.forEach(function(input) {
+    function formatNumber(value) {
+      return value.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+
+    function applyStyles() {
+      const parent = input.closest('.input-container');
+      const placeholder = parent.querySelector('.placeholder');
+
+      if (input.value) {
+        input.classList.add('filled');
+        placeholder.classList.add('filled');
+      } else {
+        input.classList.remove('filled');
+        placeholder.classList.remove('filled');
+      }
+    }
+
+    input.addEventListener('input', function() {
+      // Remove all non-digit characters
+      let value = input.value.replace(/\s+/g, '').replace(/\D/g, '');
+
+      // Ensure the value respects the min, max and maxlength constraints
+      const min = parseInt(input.getAttribute('min'), 10);
+      const max = parseInt(input.getAttribute('max'), 10);
+      const maxLength = parseInt(input.getAttribute('maxlength'), 10);
+
+      // Limit the length of the input
+      if (maxLength && value.length > maxLength) {
+        value = value.slice(0, maxLength);
+      }
+
+      // Only set value if it's within the min and max range and has maxLength digits
+      if (value && value.length === maxLength) {
+        const intValue = parseInt(value, 10);
+        if (intValue < min) {
+          value = min.toString();
+        } else if (intValue > max) {
+          value = max.toString();
+        }
+      }
+
+      input.value = value;
+      // Apply styles
+      applyStyles();
+    });
+
+    // Initial check to add filled class if input is already filled
+    applyStyles();
+  });
+}
 
